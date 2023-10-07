3b5040374818fd9e892c7503083d6ee0406911cc
Evan Senior <epicevansenior@gmail.com>
2023-10-07 18:03:39 -0400

    Added Random Button Funct.

--- gui.py
+++ gui.py
@@ -1,5 +1,33 @@
 from tkinter import *
 import tkinter.font as tkFont
+from PIL import ImageTk, Image
+import os
+
+#Fucntions for Buttons
+def randomButton():
+    root.destroy()
+    random = Tk()
+    random.title("AnimeListGUI Random")
+    random.geometry("1000x700")
+
+    img = ImageTk.PhotoImage(Image.open("Kumodesu.jpg"))
+
+    label_animeTitle = Label(random, text = "So I'm a Spider, So What?", font =  ('Arial', 18))
+    label_jpTitle = Label(random, text = "Kumo desu ga, Nani ka?", font = ('Arial', 14))
+    label_animeImg = Label(random, image = img)
+
+    label_animeTitle.place(x = 5, y = 5, anchor = 'nw')
+    label_jpTitle.place(x = 5, y = 35, anchor = 'nw')
+    label_animeImg.place(x = 5, y = 90)
+    
+    random.mainloop()
+
+def listButton():
+    root.destroy()
+    list = Tk()
+    list.title("AnimeListGUI List")
+    list.geometry("1000x700")
+    list.mainloop()
 
 #GUI itself
 root = Tk()
@@ -19,8 +47,8 @@ descriptionFont = tkFont.Font(size = 14)
 #Items to populate the frames and GUI
 label_Title = Label(root)
 label_description = Label(root)
-button_list = Button(buttonFrame, text = "List", height = 3, width = 10)
-button_random = Button(buttonFrame, text = "Random", height = 3, width = 10)
+button_list = Button(buttonFrame, text = "List", height = 3, width = 10, command = listButton)
+button_random = Button(buttonFrame, text = "Random", height = 3, width = 10, command = randomButton)
 button_airing = Button(buttonFrame, text = "Airing", height = 3, width = 10)
 label_search = Label(entryFrame, text = "Search for an anime: ")
 entry_searchBar = Entry(entryFrame)
